da9502e7f6072058ad1b7f76c0eb2dfb5dad8dc0
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-04-16 17:16:56 +0200

    Form para registrar facturas e ingresos

--- lib/src/pages/transacciones.dart
+++ lib/src/pages/transacciones.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:p2/src/filters/filterChain.dart';
 import 'package:p2/src/filters/filterConImpuestos.dart';
 import 'package:p2/src/filters/filterConversorEuros.dart';
@@ -32,8 +33,8 @@ class _TransaccionesPageState extends State<TransaccionesPage> {
 
   @override
   Widget build(BuildContext context) {
-
-    filterManager.aplicarFiltros(Provider.of<Cuenta>(context, listen: false));
+    Cuenta cuenta = Provider.of<Cuenta>(context, listen: false);
+    filterManager.aplicarFiltros(cuenta);
 
     return Scaffold(
       body: Center(
@@ -52,11 +53,126 @@ class _TransaccionesPageState extends State<TransaccionesPage> {
                 )
               ]
             ),
+            _createButtons(cuenta),
           ]
         )
       )
     );
   }
+
+  Widget _createButtons (Cuenta cuenta) {
+    return Padding(
+      padding: const EdgeInsets.all(100.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: (){
+              _decrement(cuenta);
+            },
+            tooltip: 'Decrement',
+            backgroundColor: Colors.red,
+            child: Icon(Icons.remove),
+          ),
+          FloatingActionButton(
+            onPressed: (){
+              _increment(cuenta);
+            },
+            backgroundColor: Colors.green,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _increment(Cuenta cuenta) async {
+    final controller =  TextEditingController();
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Registre un nuevo ingreso'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                new TextField(
+                  decoration: new InputDecoration(labelText: "Ingreso"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  controller: controller // Only numbers can be entered
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Registrar'),
+              onPressed: () {
+                cuenta.saldo += double.parse(controller.text);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+}
+
+  Future<void> _decrement(Cuenta cuenta) async {
+    final controller =  TextEditingController();
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Registre una nueva factura'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                new TextField(
+                  decoration: new InputDecoration(labelText: "Factura"),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  controller: controller // Only numbers can be entered
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Registrar'),
+              onPressed: () {
+                cuenta.saldo -= double.parse(controller.text);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
 }
 
 
